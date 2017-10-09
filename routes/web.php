8c3800a195f5bc241fed990c78bcb04fbf086461
midori_hust <phanthaobk@gmail.com>
2017-10-09 13:18:19 +0000

    learn route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('Route1',['as'=>'MyRoute',function(){
+    echo "Hello you!";
+}]);
+Route::get('Route2', function(){
+    echo "This is my route 2";
+})->name('MyRoute2');
+
+Route::get('Callname',function(){
+    return redirect()->route('MyRoute2');
+});
+Route::group(['middleware'=>'Mygroup'],function(){
+   Route::get('User1',function(){
+       echo "User1";
+   }) ;
+   Route::get('User2',function(){
+       echo "User2";
+   });
+   Route::get('User3',function(){
+       echo 'User3';
+   });
+   
+});
